63b46bb6f8c6417a6b68b83369ae01e9e6182b88
jaeeun <sje5149@naver.com>
2025-02-11 18:49:03 +0900

    냉장고정보까지 합치기 완, 상주는 아직x

--- src/components/Manage.jsx
+++ src/components/Manage.jsx
@@ -1,24 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import data from './data.js';
 import './Manage.css';
 
 const Manage = () => {
-  let [person, setPerson] = useState(data);
+  let [person, setPerson] = useState([]);
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    fetchPerson();
+  }, []);
+
+  const fetchPerson = async () => {
+    try {
+      const response = await axios
+        .get('http://localhost:9999/api/refrigerator')
+        .then((res) => {
+          console.log(`데이터:${res.data}`);
+          const formattedData = res.data.data.map((item) => ({
+            refrigerator_id: item.refrigerator_id,
+            person_name: item.person_name,
+            person_birthday: item.person_birthday,
+            entry_date: item.entry_date,
+            exit_date: item.exit_date,
+          }));
+          setPerson(formattedData);
+        })
+
+        .catch((res) => {
+          console.log('실패함', res.data.error);
+        });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div>
       {person.map(function (a, i) {
         return (
           <div className="personBox">
-            <p>냉장고 No: {person[i].ref}</p>
-            <h2>고인명: {person[i].name}</h2>
-            <h3>생년월일: {person[i].birth}</h3>
-            <p>입관일: {person[i].entry}</p>
-            <p>출관일: {person[i].exit}</p>
-            <p>대표 상주: {person[i].chiefMourner}</p>
-            <p>관리번호: {person[i].id}</p>
+            <p>냉장고 No: {person[i].refrigerator_id}</p>
+            <h2>고인명: {person[i].person_name}</h2>
+            <h3>생년월일: {person[i].person_birthday}</h3>
+            <p>입관일: {person[i].entry_date}</p>
+            <p>출관일: {person[i].exit_date}</p>
             <button
               onClick={() => {
                 navigate(`/setting/${i}`);
--- src/components/ManageSetting.jsx
+++ src/components/ManageSetting.jsx
@@ -1,14 +1,44 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, useLocation, useParams } from 'react-router-dom';
 import data from './data.js';
 
 const ManageSetting = () => {
-  let [person, setPerson] = useState(data);
+  let [person, setPerson] = useState([]);
+  const [loading, setLoading] = useState(true);
   const { index } = useParams();
   const i = parseInt(index, 10);
   let [modal, setModal] = useState(false);
+
   console.log('location.state:', location.state);
+
+  useEffect(() => {
+    fetchPerson();
+  }, []);
+
+  const fetchPerson = async () => {
+    try {
+      const response = await axios
+        .get('http://localhost:9999/api/refrigerator')
+        .then((res) => {
+          console.log(`데이터:${res.data}`);
+          const formattedData = res.data.data.map((item) => ({
+            refrigerator_id: item.refrigerator_id,
+            person_name: item.person_name,
+            person_birthday: item.person_birthday,
+            entry_date: item.entry_date,
+            exit_date: item.exit_date,
+          }));
+          setPerson(formattedData);
+        })
+
+        .catch((res) => {
+          console.log('실패함', res.data.error);
+        });
+    } finally {
+      setLoading(false);
+    }
+  };
 
   if (i === undefined || person[i] === undefined) {
     return <p>데이터를 불러올 수 없습니다.</p>;
@@ -16,14 +46,12 @@ const ManageSetting = () => {
   return (
     <div>
       <div className="personBox">
-        <p>관리번호: {person[i].id}</p>
-        <p>냉장고 No: {person[i].ref}</p>
-        <h3>
-          고인명: {person[i].name} 생년월일: {person[i].birth}
-        </h3>
-        <p>입관일: {person[i].entry}</p>
-        <p>출관일: {person[i].exit}</p>
-        <p>대표 상주: {person[i].chiefMourner}</p>
+        <p>냉장고 No: {person[i].refrigerator_id}</p>
+        <h3>고인명: {person[i].person_name}</h3>
+        <h3>생년월일: {person[i].person_birthday}</h3>
+        <p>입관일: {person[i].entry_date}</p>
+        <p>출관일: {person[i].exit_date}</p>
+
         <p>설정온도: 제상:</p>
 
         <button
@@ -52,25 +80,38 @@ const Modal = (props) => {
       style={{ borderBlock: '1px solid', background: 'lightblue' }}
     >
       <h4 style={{ marginLeft: '10px' }}>수정</h4>
-      <span>관리번호 : </span>
+      <span>냉장고 No : </span>
       <input
         type="text"
-        name="id"
-        placeholder={person[i].id}
+        name="ref"
+        placeholder={person[i].refrigerator_id}
         onChange={(e) => {
           setInputText(e.target.value);
           console.log(inputText);
         }}
       />
       <br />
-      <span>냉장고 No : </span>
-      <input type="text" name="ref" placeholder={person[i].ref} />
-      <br />
       <span>고인명 : </span>
-      <input type="text" name="name" placeholder={person[i].name} />
+      <input
+        type="text"
+        name="name"
+        placeholder={person[i].person_name}
+        onChange={(e) => {
+          setInputText(e.target.value);
+          console.log(inputText);
+        }}
+      />
       <br />
       <span>생년월일 : </span>
-      <input type="text" name="birth" placeholder={person[i].birth} />
+      <input
+        type="text"
+        name="birth"
+        placeholder={person[i].person_birthday}
+        onChange={(e) => {
+          setInputText(e.target.value);
+          console.log(inputText);
+        }}
+      />
       <br />
 
       <button>저장</button>
