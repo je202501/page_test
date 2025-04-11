b7094b3c58a6f8a5531c8d595bfa95f045cf6100
sje <sje5149@naver.com>
2025-04-11 21:33:35 +0900

    이미지 오류 수정

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -12,24 +12,24 @@ const Image = ({ refrigerator_id }) => {
             import.meta.env.VITE_SERVER_URL
           }:9999/api/image/?refrigerator_id=${refrigerator_id}`,
           {
-            responseType: 'blob', // 리스폰 타입을 블롭으로 받을 수 있게 설정
+            responseType: 'blob',
           }
         );
 
-        const imageUrl = URL.createObjectURL(response.data); // URL로 오는 이미지를 인식할 수 있게 블롭 데이터를 URL로 바꾸는 코드
-        console.log(imageUrl);
-        if (!imageUrl) {
-          console.error('이미지 URL을 찾을 수 없습니다.');
-          return;
-        }
-
+        const imageUrl = URL.createObjectURL(response.data);
         setImageSrc(encodeURI(imageUrl));
       } catch (error) {
         console.error('이미지 가져오기 실패:', error);
       }
     };
 
-    fetchImage();
+    fetchImage(); // 처음 실행
+
+    const interval = setInterval(() => {
+      fetchImage(); // 10초마다 새로고침
+    }, 10000);
+
+    return () => clearInterval(interval); // cleanup
   }, [refrigerator_id]);
 
   return (
